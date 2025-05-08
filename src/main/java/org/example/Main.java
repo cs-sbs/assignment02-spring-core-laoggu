bb59ad33e042e9f79444e28b687fe23a8783dd86
laogu <54233116+laoggu@users.noreply.github.com>
2025-05-08 14:37:53 +0800

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
+import org.example.service.AopDemoServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("aspects.xml");
+        AopDemoServiceImpl service = context.getBean("aopDemoService", AopDemoServiceImpl.class);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        service.doMethod1();
+        service.doMethod2();
+        try {
+            service.doMethod3();
+        } catch (Exception e) {
+//            throw new RuntimeException(e);
         }
+
     }
 }
---
+++ src/main/java/org/example/service/AopDemoServiceImpl.java
@@ -0,0 +1,23 @@
+package org.example.service;
+
+import org.springframework.stereotype.Service;
+
+
+public class AopDemoServiceImpl {
+
+    public void doMethod1() {
+        System.out.println("AopDemoServiceImpl.doMethod1");
+    }
+
+
+    public String doMethod2() {
+        System.out.println("AopDemoServiceImpl.doMethod2");
+        return "doMethod2 hello";
+    }
+
+    public String doMethod3() throws Exception {
+        System.out.println("AopDemoServiceImpl.doMethod3");
+        throw new Exception("AopDemoServiceImpl Exception");
+    }
+
+}
